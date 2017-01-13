68440c7b7fef6678c1ede382bf6bcadadcd561d3
Johnny Flame Lee <johnnyflamelee@gmail.com>
2017-01-14 12:04:48 +1300

    need to parse text

--- etude3/arithmetic.py
+++ etude3/arithmetic.py
@@ -1,4 +1,49 @@
 import sys
 
+
+input_data = ""
+target = 0
+
+
+input_figures = sys.stdin.readline()
+target = sys.stdin.read(1)
+left_to_right = True if sys.stdin.read(1) is 'L' else False
+print input_figures
+print target
+print left_to_right
+
+
+
+
+
+
+
+
+
+
+
+
+"""
 for line in sys.stdin:
-    print line 
+    input_data += line
+print input_data
+
+"""
+
+
+
+"""
+
+*assuming the input is not corrupted (Error handling later)
+
+1. add all tokens from line 1 into a list of ints
+2. add first token of line 2 to the variable target
+3. add second token of line 2 to a boolean
+"""
+
+# def calculation():
+    
+
+
+        
+    
