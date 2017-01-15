f493e687d449779820f6c930f5e07ee279b4dc9b
freakazette <frida.israelsson@gmail.com>
2017-01-16 12:37:05 +1300

    Add files via upload
    I changed it so it can take several ants at once. Added even more printing to see that the DNA sets up okay. Don't understand why it doesn't get to the right coordinates.. Maybe I misunderstood the instructions? Can you have a look when you have time..?

--- 01/Ants.java
+++ 01/Ants.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 import java.util.regex.*;
 import java.awt.Point;
 
+import java.util.Arrays;
+
 /**
  * COSC326, January 2017, Etude 1
  * Java 7
@@ -16,55 +18,61 @@ public class Ants {
         N, E, S, W;
     }
 
-    public static void run(HashMap<Character, Character[][]> dna, char defaultState, int steps){
-        HashMap<Point, Character> visitedTiles = new HashMap<Point, Character>();
-        HashMap<Character, Integer> direction = new HashMap<Character, Integer>();
-        Point currentTile = new Point();
+    public static String run(HashMap<Character,Character[][]> dna,
+                           char defaultState, int steps){
+        HashMap<Point,Character> visitedTiles = new HashMap<Point,Character>();
+        HashMap<Character,Integer> direction = new HashMap<Character,Integer>();
+        Point currentTile = new Point(0,0);
         Character state = null;
         char stepDirection = 'N';
         Character newState = null;
         int x = 0;
 
-        for(Direction d: Direction.values()){ //linking direction and position in dna line array
+        //linking direction and position in dna line array
+        for(Direction d: Direction.values()){
             direction.put(d.toString().charAt(0), x);
             x++;
         }
 
         while(steps>0){
-                   
-            state = visitedTiles.get(currentTile);
 
             System.out.println("\nwhere am I?   "+currentTile.toString());
-            System.out.println("have I been here?   "+visitedTiles.containsKey(currentTile));
-
+            System.out.println("have I been here? ->"+visitedTiles.containsKey(currentTile));
+                   
+            state = visitedTiles.get(currentTile);
 
             if(state==null){ //not visited before -> default state
                 state = defaultState;
             }
-            System.out.println("I came "+stepDirection+" and here is "+state);
+            System.out.println("I came "+stepDirection+" and here is: "+state);
+
             
             newState = dna.get(state)[direction.get(stepDirection)][1];
             stepDirection = dna.get(state)[direction.get(stepDirection)][0];
-            
+
             System.out.println("and now I go.. "+stepDirection);
-            System.out.println("this place, "+currentTile.toString()+" turns "+newState);
+            System.out.println("this place turns: "+newState);
             
             visitedTiles.put(currentTile, newState); // adds or overwrites tile state
 
             switch (stepDirection){
-                case 'N': currentTile.move((int)currentTile.getX(), (int)currentTile.getY()+1);
+                case 'N': currentTile.move((int)currentTile.getX(),
+                                           (int)currentTile.getY()+1);
                     break;
-                case 'E': currentTile.move((int)currentTile.getX()+1, (int)currentTile.getY());
+                case 'E': currentTile.move((int)currentTile.getX()+1,
+                                           (int)currentTile.getY());
                     break;
-                case 'S': currentTile.move((int)currentTile.getX(), (int)currentTile.getY()-1);
+                case 'S': currentTile.move((int)currentTile.getX(),
+                                           (int)currentTile.getY()-1);
                     break;
-                case 'W': currentTile.move((int)currentTile.getX()-1, (int)currentTile.getY());
+                case 'W': currentTile.move((int)currentTile.getX()-1,
+                                           (int)currentTile.getY());
                     break;
             }            
             steps--;
         }
         
-        System.out.println("# "+(int)currentTile.getX()+" "+(int)currentTile.getY());
+        return "# "+(int)currentTile.getX()+" "+(int)currentTile.getY();
     }
 
     public static void main(String[] args){
@@ -94,12 +102,24 @@ public class Ants {
                     dna.put(line.charAt(0), dnaLine);
                     output += line+"\n";
                     defaultState = line.charAt(0);
-                    //overwrites default state if scenario is NOT preceeded by empty line
-                } else if(line.matches("\\d+")){ // is a number
+                    // overwrites default state if scenario is NOT preceeded by
+                    // an empty line
+                } else if(line.matches("\\d+")){ //if line is a number
                     output += line;
                     System.out.println(output);
-                    run(dna, defaultState, Integer.parseInt(line));
-                    return;
+                    System.out.println(run(dna, defaultState, Integer.parseInt(line)));
+                    
+                    for(Character c: dna.keySet()){
+                        Character[][] a = dna.get(c);
+                        int i=0;
+                        for (Character[] b: a){
+                            System.out.print(c+"->"+i+" - ");
+                            System.out.println(Arrays.toString(b));
+                            i++;
+                        }
+                    }
+                        
+                    dna.clear();
                 }   
             } else {
                 line = commandLine.nextLine();
@@ -113,12 +133,12 @@ public class Ants {
                     dna.put(line.charAt(0), dnaLine);
                     output += "\n"+line+"\n";
                 } else if(line.charAt(0)=='#'){
-                    // comment
+                    //comment
                 } else if(line.matches("\\d+")){
                     output += line;
                     System.out.println(output);
-                    run(dna, defaultState, Integer.parseInt(line));
-                    return;
+                    System.out.println(run(dna, defaultState, Integer.parseInt(line)));
+                    dna.clear();
                 }   
             }
         }
