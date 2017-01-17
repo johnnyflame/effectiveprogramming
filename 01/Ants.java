0bb4cf1103e51a36336f8f0a3b09afbb84f58498
freakazette <frida.israelsson@gmail.com>
2017-01-18 12:22:23 +1300

    Add files via upload
    Output is right now! Also removed a variable that we didn't really need.. 
    Can do 100000000 steps in around 40 sec, but gets exception "java.lang.OutOfMemoryError: Java heap space" if trying to do 1000000000 steps.

--- 01/Ants.java
+++ 01/Ants.java
@@ -3,8 +3,6 @@ import java.util.HashMap;
 import java.util.regex.*;
 import java.awt.Point;
 
-import java.util.Arrays;
-
 /**
  * COSC326, January 2017, Etude 1
  * Java 7
@@ -26,7 +24,6 @@ public class Ants {
         Point nextTime;
         Character state = null;
         char stepDirection = 'N';
-        Character newState = null;
         int x = 0;
 
         //linking direction and position in dna line array
@@ -43,15 +40,14 @@ public class Ants {
                 state = defaultState;
             }
 
-            // getting new direction and state based on current state
-            newState = dna.get(state)[direction.get(stepDirection)][1];
+            //adds tile or overwrites tile state with new state
+            visitedTiles.put(currentTile, dna.get(state)[direction.get(stepDirection)][1]);
+
+            // getting new direction  based on current state
             stepDirection = dna.get(state)[direction.get(stepDirection)][0];
 
-            //adds tile or overwrites tile state
-            visitedTiles.put(currentTile, newState); 
-
             // currentTile becomes a copy of itself to not overwrite the tile
-            // stored in visitedTimes when stepping 
+            // last stored in visitedTimes when stepping 
             currentTile = new Point(currentTile);
 
             switch (stepDirection){
@@ -79,55 +75,35 @@ public class Ants {
             new HashMap<Character, Character[][]>();
         Scanner commandLine = new Scanner(System.in);
         String line;
-        Pattern dnaPattern = Pattern.compile("\\p{ASCII}\\s[NESW][NESW][NESW]"+
-                                             "[NESW]\\s\\p{ASCII}\\p{ASCII}"
-                                             +"\\p{ASCII}\\p{ASCII}");
+        //Pattern dnaPattern = Pattern.compile(".\\s[NESW][NESW][NESW]"+
+        //"[NESW]\\s....");
         Character defaultState = null;
-        String output = "";
   
         while (commandLine.hasNext()){
             line = commandLine.nextLine();
-            Matcher m = dnaPattern.matcher(line);
+            //Matcher m = dnaPattern.matcher(line);
 
             if(line.length()!=0){ //if not an empty line
                 if (line.charAt(0)=='#'){
                     //this is a comment
-                } else if(m.matches()){
-                    Character[][] dnaLine = new Character[4][2];
-                    for(int i=0; i<4; i++){
-                        dnaLine[i][0] = line.charAt(i+2);
-                        dnaLine[i][1] = line.charAt(i+7);
-                    }
-                    dna.put(line.charAt(0), dnaLine);
-                    output += line+"\n";
-                    defaultState = line.charAt(0);
-                    // overwrites default state if scenario is NOT preceeded by
-                    // an empty line
                 } else if(line.matches("\\d+")){ //if line is a number
-                    output += line;
-                    System.out.println(output);
-                    System.out.println(run(dna, defaultState, Integer.parseInt(line)));  
+                    System.out.println(line);
+                    System.out.println(run(dna, defaultState,
+                                           Integer.parseInt(line)));
+                    defaultState = null;
                     dna.clear();
-                }   
-            } else {
-                line = commandLine.nextLine();
-                m = dnaPattern.matcher(line);
-                if(m.matches()){
+                } else {
                     Character[][] dnaLine = new Character[4][2];
                     for(int i=0; i<4; i++){
                         dnaLine[i][0] = line.charAt(i+2);
                         dnaLine[i][1] = line.charAt(i+7);
                     }
                     dna.put(line.charAt(0), dnaLine);
-                    output += "\n"+line+"\n";
-                } else if(line.charAt(0)=='#'){
-                    //comment
-                } else if(line.matches("\\d+")){
-                    output += line;
-                    System.out.println(output);
-                    System.out.println(run(dna, defaultState, Integer.parseInt(line)));
-                    dna.clear();
-                }   
+                    if(defaultState==null){
+                        defaultState = line.charAt(0);
+                    }
+                    System.out.println(line);
+                }
             }
         }
     }
